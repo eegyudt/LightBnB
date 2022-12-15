e751e01d88918e8155e98647ade8e013edc77e05
Eszter Egyud <eszter.egyud@telus.com>
2022-12-14 22:57:32 -0700

    Add  LightBnB_WebApp project folder

--- 1_queries/03_listings_by_city.sql
+++ 1_queries/03_listings_by_city.sql
@@ -6,19 +6,20 @@
 -- Only show listings that have a rating >= 4 stars.
 
 
--- SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating
--- FROM properties
--- JOIN property_reviews ON property_reviews.property_id = properties.id
--- WHERE city = 'Vancouver' and property_reviews.rating >= 4
--- GROUP BY properties.id;
+SELECT properties.id, title, cost_per_night, AVG(property_reviews.rating) AS average_rating
+FROM properties
+JOIN property_reviews ON property_reviews.property_id = properties.id
+WHERE city = 'Vancouver' and property_reviews.rating >= 4
+GROUP BY properties.id
+LIMIT 10;
 
 -- COMPASS solution:
 
-SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
-FROM properties
-LEFT JOIN property_reviews ON properties.id = property_id
-WHERE city LIKE '%ancouv%'
-GROUP BY properties.id
-HAVING avg(property_reviews.rating) >= 4
-ORDER BY cost_per_night
-LIMIT 10;
+-- SELECT properties.id, title, cost_per_night, avg(property_reviews.rating) as average_rating
+-- FROM properties
+-- LEFT JOIN property_reviews ON properties.id = property_id
+-- WHERE city LIKE '%ancouv%'
+-- GROUP BY properties.id
+-- HAVING avg(property_reviews.rating) >= 4
+-- ORDER BY cost_per_night
+-- LIMIT 10;
